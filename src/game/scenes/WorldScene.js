a50e089a8cbacaaa70946f9b1517ce5c80e356d1
axell152 <axell.nicol@outlook.fr>
2026-09-08 17:13:21 +0200

    Update WorldScene.js

--- src/game/scenes/WorldScene.js
+++ src/game/scenes/WorldScene.js
@@ -17,7 +17,7 @@ export default class WorldScene extends Phaser.Scene {
 
   preload() {
     // Chargement direct de l'image de la taverne
-    this.load.image("tavern", "assets/tavern.jpg");
+    this.load.image("taverne", "assets/taverne.jpg");
   }
   
   init(data) {
