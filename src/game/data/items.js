baab6b34bc047ac69d0118a9732de5318e9948be
axell152 <axell.nicol@outlook.fr>
2026-09-10 16:51:44 +0200

    Add files via upload

---
+++ src/game/data/items.js
@@ -0,0 +1,19 @@
+export const ITEMS = {
+  potion: {
+    id: "potion",
+    name: "Potion",
+    description: "Restaure 40 PV.",
+    heal: 40,
+  },
+  superPotion: {
+    id: "superPotion",
+    name: "Super Potion",
+    description: "Restaure 70 PV.",
+    heal: 70,
+  },
+};
+
+export const STARTING_ITEMS = {
+  potion: 3,
+  superPotion: 0,
+};
