f2442a5ab21aa4600af95dfe1c6ef678fed214b5
axell152 <axell.nicol@outlook.fr>
2026-09-11 08:02:59 +0200

    Update BattleScene.js

--- src/game/scenes/BattleScene.js
+++ src/game/scenes/BattleScene.js
@@ -101,12 +101,12 @@ export default class BattleScene extends Phaser.Scene {
 
   drawCombatants() {
     const enemyId = this.targetCharacterId || "marineRecrue";
-    this.enemySprite = this.add.sprite(760, 245, spriteKey(enemyId), 1).setScale(7).setOrigin(0.5, 0.82).setDepth(10);
+    this.enemySprite = this.add.sprite(760, 245, spriteKey(enemyId), 0).setScale(7).setOrigin(0.5, 0.82).setDepth(10);
     this.enemyNameText = this.add.text(480, 72, "", { fontFamily: "monospace", fontSize: "21px", color: "#ead9b8" });
     this.enemyHpBarBg = this.add.rectangle(480, 110, 300, 20, 0x161616).setOrigin(0, 0.5);
     this.enemyHpBar = this.add.rectangle(482, 110, 296, 16, 0xc0392b).setOrigin(0, 0.5);
 
-    this.playerSprite = this.add.sprite(240, 470, spriteKey(this.player.crewId), 1).setScale(7).setOrigin(0.5, 0.82).setDepth(10);
+    this.playerSprite = this.add.sprite(240, 470, spriteKey(this.player.crewId), 0).setScale(7).setOrigin(0.5, 0.82).setDepth(10);
     this.playerNameText = this.add.text(300, 495, "", { fontFamily: "monospace", fontSize: "21px", color: "#ead9b8" });
     this.playerHpBarBg = this.add.rectangle(300, 533, 300, 20, 0x161616).setOrigin(0, 0.5);
     this.playerHpBar = this.add.rectangle(302, 533, 296, 16, 0x27ae60).setOrigin(0, 0.5);
@@ -199,7 +199,7 @@ export default class BattleScene extends Phaser.Scene {
     this.clearInterfaceElements(); this.locked = true;
     const old = this.player;
     this.player = newMember;
-    this.playerSprite.setTexture(spriteKey(newMember.crewId), 1);
+    this.playerSprite.setTexture(spriteKey(newMember.crewId), 0);
     this.setLog(`${old.name} ${old.hp <= 0 ? "est K.O." : "revient à bord"} ! ${newMember.name} prend sa place.`);
     this.refreshBars();
     this.playerSprite.setAlpha(0.4);
