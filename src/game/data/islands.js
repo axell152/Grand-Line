99f12c121ab0b655965199623bddd0a5ef9e439e
axell152 <axell.nicol@outlook.fr>
2026-09-16 09:13:02 +0200

    Update islands.js

--- src/game/data/islands.js
+++ src/game/data/islands.js
@@ -73,7 +73,7 @@ export const ISLANDS = {
       team: [
         { characterId: "marineRecrue", level: 4 },
         { characterId: "marineRecrue", level: 5 },
-        { characterId: "officierMarine", level: 8, isBoss: true },
+        { characterId: "officierMarine", level: 1, isBoss: true },
       ],
     },
     wildZones: [
