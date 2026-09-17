09b0ea11b22e411a861d1e81e50c90fbe50901b5
axell152 <axell.nicol@outlook.fr>
2026-09-17 11:17:57 +0200

    Update WorldScene.js

--- src/game/scenes/WorldScene.js
+++ src/game/scenes/WorldScene.js
@@ -901,6 +901,7 @@ if (island.boss) {
     arlong: 0.8,
     officierMarine: 2.5,
     pirateRival: 2.5,
+    colonelMorgan: 0.8,
   };
 
   this.bossSprite.setScale(bossScales[boss.enemyId] ?? 2.5);
